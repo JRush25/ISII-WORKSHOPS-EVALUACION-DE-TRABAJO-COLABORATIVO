634e3a669d3326a6b7b8c73e88f4872b9cbc7b75
Joselyne Torres <joselynetorres25@hotmail.com>
2022-10-21 22:30:58 -0500

    Cambios CheckStyle

--- ec.edu.espol.workshops/lib/src/main/java/ec/edu/espol/workshops/CarInsurance.java
+++ ec.edu.espol.workshops/lib/src/main/java/ec/edu/espol/workshops/CarInsurance.java
@@ -1,77 +1,88 @@
 package ec.edu.espol.workshops;
 
 import java.util.Scanner;
-
+/**
+ * @class CarInsurance
+ * Clase principal
+ */
 public class CarInsurance {
-	/**
-	 * @field premiumBase
-	 * Precio Base de Premium
-	 */
-	private static double premiumBase = 500;
-	
-	public static double calculatePremiumTotal(int age, char gender, boolean status) {
-		if(age<25 && gender =='M' && !status) {
-			return premiumBase+1500;
-		}else if (gender == 'F' || status) {
-			return premiumBase - 200;
-		}else if (age>=45 && age <65) {
-			return premiumBase - 100;
-		}else
-			return premiumBase;
-	}
-	public static void main (String[] args) {
-		int opGender; 
-		int opstatus;
-		String oplicense;
-		Customer customer = new Customer();
-		Scanner sc = new Scanner(System.in);
-		System.out.println("*****CAR INSURANCE*****");
-		System.out.println("Enter the customer's age:");
-		customer.setAge(sc.nextInt());
-		do{
-		System.out.println("Select the customer's gender:");
-		System.out.println("1) Female");
-		System.out.println("2) Male");
-		opGender = sc.nextInt();
-		if (opGender==1) {
-			customer.setGender('F');
-		} else if (opGender==2) {
-			customer.setGender('M');
-		}else {
-			System.out.println("Enter a valid option.");
-		}
-		}while(opGender != 1  && opGender != 2);
-		do{
-		System.out.println("Select the customer's marital Status:");
-		System.out.println("1) Married");
-		System.out.println("2) Not married");
-		opstatus = sc.nextInt();
-		if (opstatus==1) {
-			customer.setMaritalStatus(true);
-		} else if (opstatus==2) {
-			customer.setMaritalStatus(false);
-		}else {
-			System.out.println("Enter a valid option.");
-		}
-		}while(opstatus != 1 && opstatus != 2);
-		do{
-			System.out.println("Does the customer have a valid license? Y/N:");
-			oplicense = sc.nextLine();
-			oplicense = oplicense.toUpperCase();
-			if (oplicense.equals("Y")) {
-				customer.setValidLicense(true);
-			} else if (oplicense.equals("N")) {
-				customer.setValidLicense(false);
-			}else {
-				System.out.println("Enter a valid option.");
-			}
-			}while(!oplicense.equals("Y") && !oplicense.equals("N"));
-		
-		if (customer.validatePolicies(customer.getAge(), customer.isValidLicense())){
-			System.out.print("The premium total is: ");
-			System.out.print(calculatePremiumTotal(customer.getAge(),customer.getGender(),customer.isMaritalStatus()));
-		}else {
-			System.out.println("Customer does not comply with policies");
-		}
-	}
+/**
+ * @field premiumBase
+ * Precio Base de Premium
+ */
+  private static double premiumBase = 500;
+/**
+ * @method calculatePremiumTotal
+ * Metodo para el calculo total
+ */
+  public static double calculatePremiumTotal(int age, char gender, boolean status) {
+    if(age <25 && gender =='M' && !status) {
+return premiumBase +1500;
+    } else if (gender == 'F' || status) {
+return premiumBase - 200;
+    } else if (age >=45 && age <65) {
+return premiumBase - 100;
+    } else
+return premiumBase;
+  }
+
+/**
+ * 
+ * @param args
+ */
+  public static void main (String[] args) {
+int opGender; 
+int opstatus;
+String oplicense;
+Customer customer = new Customer();
+Scanner sc = new Scanner(System.in);
+System.out.println("*****CAR INSURANCE*****");
+System.out.println("Enter the customer's age:");
+customer.setAge(sc.nextInt());
+    do {
+System.out.println("Select the customer's gender:");
+System.out.println("1) Female");
+System.out.println("2) Male");
+opGender = sc.nextInt();
+      if (opGender ==1) {
+customer.setGender('F');
+      } else if (opGender ==2) {
+customer.setGender('M');
+      } else {
+System.out.println("Enter a valid option.");
+      }
+    } while(opGender != 1  && opGender != 2);
+    do {
+System.out.println("Select the customer's marital Status:");
+System.out.println("1) Married");
+System.out.println("2) Not married");
+opstatus = sc.nextInt();
+      if (opstatus ==1) {
+customer.setMaritalStatus(true);
+      } else if (opstatus ==2) {
+customer.setMaritalStatus(false);
+      } else {
+System.out.println("Enter a valid option.");
+      }
+    } while(opstatus != 1 && opstatus != 2);
+    do {
+System.out.println("Does the customer have a valid license? Y/N:");
+oplicense = sc.nextLine();
+oplicense = oplicense.toUpperCase();
+      if (oplicense.equals("Y")) {
+customer.setValidLicense(true);
+      } else if (oplicense.equals("N")) {
+customer.setValidLicense(false);
+      } else {
+System.out.println("Enter a valid option.");
+      }
+    } while(!oplicense.equals("Y") && !oplicense.equals("N"));
+
+    if (customer.validatePolicies(customer.getAge(), customer.isValidLicense())) {
+System.out.print("The premium total is: ");
+System.out.print(calculatePremiumTotal(customer.getAge(),customer.getGender(),customer.isMaritalStatus()));
+    } else {
+System.out.println("Customer does not comply with policies");
+    }
+  }
 }
--- ec.edu.espol.workshops/lib/src/main/java/ec/edu/espol/workshops/Customer.java
+++ ec.edu.espol.workshops/lib/src/main/java/ec/edu/espol/workshops/Customer.java
@@ -5,73 +5,76 @@ package ec.edu.espol.workshops;
  * Cliente
  */
 public class Customer {
-	/**
-	 * @field age
-	 * Edad
-	 */
-	private int age;
-	/**
-	 * @field gender
-	 * Genero
-	 */
-	private char gender;
-	/**
-	 * @field maritalStatus
-	 * Estado Civil
-	 */
-	private boolean maritalStatus;
-	/**
-	 * @field validLicense
-	 * Licencia valida
-	 */
-	private boolean validLicense;
-	
-	public boolean isValidLicense() {
-		return validLicense;
-	}
+/**
+* @field age
+* Edad
+*/
+  private int age;
+/**
+* @field gender
+* Genero
+*/
+  private char gender;
+/**
+* @field maritalStatus
+* Estado Civil
+*/
+  private boolean maritalStatus;
+/**
+* @field validLicense
+* Licencia valida
+*/
+  private boolean validLicense;
 
-	public void setValidLicense(boolean validLicense) {
-		this.validLicense = validLicense;
-	}
-	
-	/**
-	 * @constructor Customer
-	 * Cliente
-	 */
-	public Customer() {
-		// The explicit constructor is here, so that it is possible to provide Javadoc. 
-	}
-	
-	public int getAge() {
-		return age;
-	}
-	public void setAge(int age) {
-		this.age = age;
-	}
-	public char getGender() {
-		return gender;
-	}
-	public void setGender(char gender) {
-		this.gender = gender;
-	}
-	public boolean isMaritalStatus() {
-		return maritalStatus;
-	}
-	public void setMaritalStatus(boolean maritalStatus) {
-		this.maritalStatus = maritalStatus;
-	}
-	
-	
-	/**
-	 * @metodo validatePolicies
-	 * Valida las politicas
-	 */
-	public boolean validatePolicies(int age, boolean license) {
-		if (license && age < 80) {
-			return true;
-		}else {
-			return false;
-		}
-	}
-	
+  public boolean isValidLicense() {
+return validLicense;
+  }
+
+  public void setValidLicense(boolean validLicense) {
+this.validLicense = validLicense;
+  }
+/**
+* @constructor Customer
+* Cliente
+*/
+
+  public Customer() {
+// The explicit constructor is here, so that it is possible to provide Javadoc. 
+  }
+
+  public int getAge() {
+return age;
+  }
+
+  public void setAge(int age) {
+this.age = age;
+  }
+
+  public char getGender() {
+return gender;
+  }
+
+  public void setGender(char gender) {
+this.gender = gender;
+  }
+
+  public boolean isMaritalStatus() {
+return maritalStatus;
+  }
+
+  public void setMaritalStatus(boolean maritalStatus) {
+this.maritalStatus = maritalStatus;
+  }
+/**
+* @metodo validatePolicies
+* Valida las politicas
+*/
+
+  public boolean validatePolicies(int age, boolean license) {
+    if (license && age < 80) {
+return true;
+    } else {
+return false;
+    }
+  }
 }
